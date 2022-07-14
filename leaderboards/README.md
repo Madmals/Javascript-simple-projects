5ec82b9a8762c3882940948d88df19b236501bb2
sambel_petai <madmalzb@gmail.com>
2022-07-14 13:36:48 +0800

    working-deploy

--- leaderboards/README.md
+++ leaderboards/README.md
@@ -3,3 +3,5 @@ list top 10 movie list and list tag
 
 <img src="./assets/leaderboard_movie.gif">
 
+# future
+1. lazy loading or image loading
--- leaderboards/cypress.config.js
+++ leaderboards/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://127.0.0.1:3000",
+    baseUrl: "https://remarkable-dango-daa572.netlify.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
